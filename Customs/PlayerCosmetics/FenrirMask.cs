25b48adea8841b430e1d621f0eda63d34da06ed3
Lachlan Leone <lachlan@starfluxgames.com>
2025-01-18 17:44:22 +1100

    Mod Repairing & Cleanup

---
+++ Customs/PlayerCosmetics/FenrirMask.cs
@@ -0,0 +1,22 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using UnityEngine;
+
+namespace FenrirMask.Customs.PlayerCosmetics
+{
+    public class FenrirMask : CustomPlayerCosmetic
+    {
+        public override string UniqueNameID => "FenrirMask";
+        public override CosmeticType CosmeticType => CosmeticType.Hat;
+        public override GameObject Visual => Mod.Bundle.LoadAsset<GameObject>("FenrirMask").AssignMaterialsByNames();
+        public override bool BlockHats => true;
+
+        public override void OnRegister(PlayerCosmetic gameDataObject)
+        {
+            PlayerOutfitComponent playerOutfitComponent = gameDataObject.Visual.AddComponent<PlayerOutfitComponent>();
+            playerOutfitComponent.Renderers.Add(GameObjectUtils.GetChildObject(gameDataObject.Visual, "Fenrir Mask").GetComponent<SkinnedMeshRenderer>());
+        }
+    }
+}
