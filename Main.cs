494a0ad310c08c4c076b43a11537bf5d9da5bebf
Lalyth <phil.lam1989@gmail.com>
2025-01-18 00:56:03 -0500

    Init

---
+++ Main.cs
@@ -0,0 +1,70 @@
+using Kitchen;
+using KitchenLib;
+using KitchenMods;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using KitchenLib.Event;
+using KitchenLib.Customs;
+using System.Linq;
+using System.IO;
+using KitchenFenrirMask.Hat;
+
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenFenrirMask
+{
+    public class Main : BaseMod
+    {
+        // guid must be unique and is recommended to be in reverse domain name notation
+        // mod name that is displayed to the player and listed in the mods menu
+        // mod version must follow semver e.g. "1.2.3"
+        public const string MOD_GUID = "Lalyth.PlateUp.FenrirMask";
+        public const string MOD_NAME = "FenrirMask";
+        public const string MOD_VERSION = "0.0.1";
+        public const string MOD_AUTHOR = "Lalyth";
+        public const string MOD_GAMEVERSION = ">=1.1.3";
+        // Game version this mod is designed for in semver
+        // e.g. ">=1.1.1" current and all future
+        // e.g. ">=1.1.1 <=1.2.3" for all from/until
+
+        public Main() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+
+        public static AssetBundle bundle;
+
+        protected override void OnInitialise()
+        {
+            // For log file output so the official plateup support staff can identify if/which a mod is being used
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+        }
+
+        private void AddGameData()
+        {
+            LogInfo("Attempting to register game data...");
+
+            // Outfits
+            AddGameDataObject<FenrirMask>();
+
+            LogInfo("Done loading game data.");
+        }
+
+        protected override void OnPostActivate(Mod mod)
+        {
+            LogInfo("Attempting to load asset bundle...");
+            bundle = mod.GetPacks<AssetBundleModPack>().SelectMany(e => e.AssetBundles).ToList()[0];
+            LogInfo("Done loading asset bundle.");
+
+            AddGameData();
+        }
+
+        #region Logging
+        // You can remove this, I just prefer a more standardized logging
+        public static void LogInfo(string _log) { Debug.Log($"{MOD_NAME} " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"{MOD_NAME} " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"{MOD_NAME} " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
+    }
+
+}
